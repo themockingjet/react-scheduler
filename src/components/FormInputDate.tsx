bf9380e6e9cf506dc883824446dffec905b820d9
themockingjet <jwoot001@gmail.com>
2023-08-09 10:48:38 +0800

    add date and time function

--- src/components/FormInputDate.tsx
+++ src/components/FormInputDate.tsx
@@ -3,9 +3,11 @@
 //
 import DatePicker from "react-datepicker";
 import cn from "classnames";
-import { useState } from "react";
-import { useFormContext, useForm, Controller } from "react-hook-form";
+import { Controller, useFormContext } from "react-hook-form";
 import { findInputError, isFormInvalid } from "../utils";
+import { useOccupiedDates } from "../hooks/useOccupiedDates";
+import { useOccupiedTimes } from "../hooks/useOccupiedTimes";
+import { useEffect } from "react";
 
 interface FormInputDateProps {
     //
@@ -18,25 +20,33 @@ interface FormInputDateProps {
             message: string;
         };
     };
-
+    date?: Date | null;
     divClassName?: string;
     labelClassName?: string;
     inputClassName?: string;
+    onDateChange?: (e: any) => void;
 }
 
 const FormInputDate = ({ id, name, label, validation, ...props }: FormInputDateProps) => {
     const divCss = "flex flex-col w-full  w-full px-3 py-2 h-26";
     const labelCss = "w-full text-base";
-    const inputCss = "w-full px-3 py-2 shadow-sm border focus:outline-none h-12";
+    const inputCss = "w-full px-3 py-2 shadow-sm border focus:outline-none h-12 disabled:bg-gray-200";
 
     const {
         control,
-        register,
         formState: { errors },
     } = useFormContext();
 
     const inputErrors: any = findInputError(errors, name);
     const isInvalid = isFormInvalid(inputErrors);
+
+    const { dateFilter } = useOccupiedDates();
+    const date = props.date;
+    const { timeFilter, fetchReservationsByDate, filterPassedTime } = useOccupiedTimes(date);
+
+    useEffect(() => {
+        fetchReservationsByDate();
+    }, [date]);
 
     return (
         <>
@@ -48,11 +58,19 @@ const FormInputDate = ({ id, name, label, validation, ...props }: FormInputDateP
                     name={name}
                     control={control}
                     rules={validation}
-                    render={({ field: { ref, onChange, value, ...field } }) => (
+                    render={({ field: { onChange, value, ...field } }) => (
                         <DatePicker
                             className={cn(inputCss, props.inputClassName)}
-                            onChange={onChange}
+                            onChange={(e: Date) => {
+                                onChange(e);
+                                props.onDateChange && props.onDateChange(e);
+                            }}
                             selected={value}
+                            disabled={name === "time" && date === null}
+                            excludeDates={name === "date" ? dateFilter : []}
+                            excludeTimes={name === "time" ? timeFilter : []}
+                            filterTime={name === "time" ? filterPassedTime : undefined}
+                            minDate={name === "time" ? date : new Date()}
                             {...field}
                             {...props}
                         />
